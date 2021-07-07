f582bac8be45c430988d01a6d4cfa52563d2246f
nandozanutto <nando.zanutto@gmail.com>
2021-07-07 13:03:52 -0300

    color change

--- avatarScript.js
+++ avatarScript.js
@@ -1,5 +1,5 @@
 function changeAsset(isUp, asset){
-    const limits = {oculos:6, calcado:2, calca:3, blusa:8, brinco:2, boca:4, nariz:2, sobrancelha:4};
+    const limits = {oculos:7, calcado:2, calca:3, blusa:8, brinco:3, boca:4, nariz:2, sobrancelha:4};
     if(!asset.localeCompare("cabelo")){
         var cabeloSrc = document.getElementById("cabelo").src;
         var numPos = cabeloSrc.indexOf("cabelo");
@@ -23,31 +23,32 @@ function changeAsset(isUp, asset){
         var aux = corpoSrc.charAt(numPos + 6);
         if(aux === "f"){
             var newSrc = corpoSrc.slice(0,numPos);
-            var aux2 = newSrc.concat("corpo_m_cor1.png");
-            document.getElementById("corpo").src = aux2;
-
-            var aux3 = newSrc.concat("corpo_m_blusa1_cor1.png");
-            document.getElementById("blusa").src = aux3;
-            
-            var aux4 = newSrc.concat("corpo_m_calca1_cor1.png");             
-            document.getElementById("calca").src = aux4;
-            
-            var aux5 = newSrc.concat("corpo_m_calcado1_cor1.png");
-            document.getElementById("calcado").src = aux5;
+            document.getElementById("corpo").src = newSrc + "corpo_m_cor1.png";
+            document.getElementById("blusa").src = newSrc + "corpo_m_blusa1_cor1.png";
+            document.getElementById("calca").src = newSrc + "corpo_m_calca1_cor1.png";
+            document.getElementById("calcado").src = newSrc + "corpo_m_calcado1_cor1.png";
         }else{
             var newSrc = corpoSrc.slice(0,numPos);
-            var aux2 = newSrc.concat("corpo_f_cor1.png");
-            document.getElementById("corpo").src = aux2;
-
-            var aux3 = newSrc.concat("corpo_f_blusa1_cor1.png");
-            document.getElementById("blusa").src = aux3;
-            
-            var aux4 = newSrc.concat("corpo_f_calca1_cor1.png");             
-            document.getElementById("calca").src = aux4;
-            
-            var aux5 = newSrc.concat("corpo_f_calcado1_cor1.png");
-            document.getElementById("calcado").src = aux5;
+            document.getElementById("corpo").src = newSrc + "corpo_f_cor1.png";
+            document.getElementById("blusa").src = newSrc + "corpo_f_blusa1_cor1.png";
+            document.getElementById("calca").src = newSrc + "corpo_f_calca1_cor1.png";
+            document.getElementById("calcado").src = newSrc + "corpo_f_calcado1_cor1.png";
         }
+    }else if(!asset.localeCompare("blusa")){
+        var assetSrc = document.getElementById(asset).src;
+        var numPos = assetSrc.indexOf(asset);
+        var aux = assetSrc.charAt(numPos + asset.length);
+        var number = parseInt(aux);
+        number++;
+        if(number>limits[asset])
+            number=1;
+        var aux2 = asset.concat(aux);
+        var newSrc = assetSrc.replace(aux2, asset + number);//trocando blusa
+        //**************/
+        var numPosCor = assetSrc.indexOf("_cor");
+        var aux = assetSrc.charAt(numPosCor + 4);
+        var aux2 = ("_cor").concat(aux);
+        document.getElementById(asset).src = newSrc.replace(aux2, "_cor" + 1);//resetando cor
     }
 
     else{
@@ -61,10 +62,47 @@ function changeAsset(isUp, asset){
         var aux2 = asset.concat(aux);
         document.getElementById(asset).src = assetSrc.replace(aux2, asset + number);
     }
-
-
-    
+ 
 }
+function changeColor(asset){
+    const colorLimits = {cabelo:6, blusa:3, calca:3, calcado:3, corpo:5};
+    var limit = colorLimits[asset];
+    //blusaF = 3 (blusaF < 4)
+    //blusaF = 1 (blusaF >= 4)
+
+    //blusaM = 3 (blusaM < 3)
+    //blusaM = 1 (blusaM >= 3)
+
+    var assetSrc = document.getElementById(asset).src;
+    var numPos = assetSrc.indexOf("_cor");
+
+    if(!asset.localeCompare("blusa")){
+        var corpoSrc = document.getElementById("corpo").src;
+        var numPosC = corpoSrc.indexOf("corpo");
+        var corpoType = corpoSrc.charAt(numPosC + 6);
+        //**************** */
+        var blusaSrc = document.getElementById("blusa").src;
+        var numPosB = blusaSrc.indexOf("blusa");
+        var auxB = assetSrc.charAt(numPosB + 5);
+        var numberB = parseInt(auxB);
+        if(corpoType === "f" && numberB > 3)
+            return;
+        if(corpoType === "m" && numberB > 2)
+            return;
+        
+    }
+
+    var aux = assetSrc.charAt(numPos + 4);
+    var number = parseInt(aux);
+    number++;
+    if(number>limit)
+        number=1;
+    var aux2 = ("_cor").concat(aux);
+    document.getElementById(asset).src = assetSrc.replace(aux2, "_cor" + number);
+}
+
+
+
     // var n = str.length - 1;
     // if(isUp){
     //     var res = parseInt(str.charAt(n)) + 1

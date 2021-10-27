71e10ab44e321eba063067cdbabab42a69b4c5ce
nandozanutto <nando.zanutto@gmail.com>
2021-10-27 17:15:02 -0300

    .

--- code1.js
+++ code1.js
@@ -1,16 +1,62 @@
 gdjs.derrotaCode = {};
+gdjs.derrotaCode.GDGameOverObjects1= [];
+gdjs.derrotaCode.GDGameOverObjects2= [];
+gdjs.derrotaCode.GDbackgroundObjects1= [];
+gdjs.derrotaCode.GDbackgroundObjects2= [];
+gdjs.derrotaCode.GDTelaGameOverObjects1= [];
+gdjs.derrotaCode.GDTelaGameOverObjects2= [];
+gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1= [];
+gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects2= [];
 
 gdjs.derrotaCode.conditionTrue_0 = {val:false};
 gdjs.derrotaCode.condition0IsTrue_0 = {val:false};
 
 
-gdjs.derrotaCode.eventsList0 = function(runtimeScene) {
+gdjs.derrotaCode.mapOfGDgdjs_46derrotaCode_46GDTelaNaoFoidessaVezObjects1Objects = Hashtable.newFrom({"TelaNaoFoidessaVez": gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1});gdjs.derrotaCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+{
+}
+
+}
+
+
+{
+
+
+{
+gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : runtimeScene), gdjs.derrotaCode.mapOfGDgdjs_46derrotaCode_46GDTelaNaoFoidessaVezObjects1Objects, 0, 0, "");
+}{for(var i = 0, len = gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1.length ;i < len;++i) {
+    gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1[i].setWidth(0.9 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene));
+}
+}{for(var i = 0, len = gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1.length ;i < len;++i) {
+    gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1[i].setHeight(0.9 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene));
+}
+}{for(var i = 0, len = gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1.length ;i < len;++i) {
+    gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1[i].setCenterPositionInScene(0.5 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene),0.5 * gdjs.evtTools.window.getGameResolutionHeight(runtimeScene));
+}
+}}
+
+}
+
 
 };
 
 gdjs.derrotaCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
+gdjs.derrotaCode.GDGameOverObjects1.length = 0;
+gdjs.derrotaCode.GDGameOverObjects2.length = 0;
+gdjs.derrotaCode.GDbackgroundObjects1.length = 0;
+gdjs.derrotaCode.GDbackgroundObjects2.length = 0;
+gdjs.derrotaCode.GDTelaGameOverObjects1.length = 0;
+gdjs.derrotaCode.GDTelaGameOverObjects2.length = 0;
+gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects1.length = 0;
+gdjs.derrotaCode.GDTelaNaoFoidessaVezObjects2.length = 0;
 
 gdjs.derrotaCode.eventsList0(runtimeScene);
 return;
--- code2.js
+++ code2.js
@@ -3,20 +3,32 @@ gdjs.vitoriaCode.GDbackgroundObjects1= [];
 gdjs.vitoriaCode.GDbackgroundObjects2= [];
 gdjs.vitoriaCode.GDNewObjectObjects1= [];
 gdjs.vitoriaCode.GDNewObjectObjects2= [];
-gdjs.vitoriaCode.GDNewObject2Objects1= [];
-gdjs.vitoriaCode.GDNewObject2Objects2= [];
+gdjs.vitoriaCode.GDTelaParabensObjects1= [];
+gdjs.vitoriaCode.GDTelaParabensObjects2= [];
 
 gdjs.vitoriaCode.conditionTrue_0 = {val:false};
 gdjs.vitoriaCode.condition0IsTrue_0 = {val:false};
 
 
-gdjs.vitoriaCode.eventsList0 = function(runtimeScene) {
+gdjs.vitoriaCode.mapOfGDgdjs_46vitoriaCode_46GDTelaParabensObjects1Objects = Hashtable.newFrom({"TelaParabens": gdjs.vitoriaCode.GDTelaParabensObjects1});gdjs.vitoriaCode.eventsList0 = function(runtimeScene) {
 
 {
 
 
 {
+gdjs.vitoriaCode.GDTelaParabensObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : runtimeScene), gdjs.vitoriaCode.mapOfGDgdjs_46vitoriaCode_46GDTelaParabensObjects1Objects, 0, 0, "");
+}{for(var i = 0, len = gdjs.vitoriaCode.GDTelaParabensObjects1.length ;i < len;++i) {
+    gdjs.vitoriaCode.GDTelaParabensObjects1[i].setHeight(0.9 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene));
 }
+}{for(var i = 0, len = gdjs.vitoriaCode.GDTelaParabensObjects1.length ;i < len;++i) {
+    gdjs.vitoriaCode.GDTelaParabensObjects1[i].setWidth(0.9 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene));
+}
+}{for(var i = 0, len = gdjs.vitoriaCode.GDTelaParabensObjects1.length ;i < len;++i) {
+    gdjs.vitoriaCode.GDTelaParabensObjects1[i].setCenterPositionInScene(0.5 * gdjs.evtTools.window.getGameResolutionWidth(runtimeScene),0.5 * gdjs.evtTools.window.getGameResolutionHeight(runtimeScene));
+}
+}}
 
 }
 
@@ -30,8 +42,8 @@ gdjs.vitoriaCode.GDbackgroundObjects1.length = 0;
 gdjs.vitoriaCode.GDbackgroundObjects2.length = 0;
 gdjs.vitoriaCode.GDNewObjectObjects1.length = 0;
 gdjs.vitoriaCode.GDNewObjectObjects2.length = 0;
-gdjs.vitoriaCode.GDNewObject2Objects1.length = 0;
-gdjs.vitoriaCode.GDNewObject2Objects2.length = 0;
+gdjs.vitoriaCode.GDTelaParabensObjects1.length = 0;
+gdjs.vitoriaCode.GDTelaParabensObjects2.length = 0;
 
 gdjs.vitoriaCode.eventsList0(runtimeScene);
 return;
